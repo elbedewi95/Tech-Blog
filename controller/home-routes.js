3bb7d08c781051c19e28fe5d2c9476dcf60ec9cc
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-11 10:05:49 -0700

    get route for all posts to render on homepage

---
+++ controller/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment} = require('../models');
+
+
+        router.get("/", async (req, res) => {
+            try {
+                let postData = await Post.findAll({
+                    include: [{
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+                })
+        
+               
+                const posts = postData.map((post) => post.get({ plain: true }));
+                res.render("homepage", { posts })
+              
+        
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        })
+    
