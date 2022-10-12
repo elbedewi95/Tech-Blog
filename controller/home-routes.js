7742c4a041538413ae129507995c1b5122d04d5e
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-11 17:40:15 -0700

    one post route

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -23,11 +23,57 @@ const { User, Post, Comment} = require('../models');
         
                
                 const posts = postData.map((post) => post.get({ plain: true }));
-                res.render("homepage", { posts })
+                res.render("homepage", { posts, loggedIn: req.session.loggedIn })
               
         
             } catch (err) {
                 res.status(500).json(err)
             }
         })
+
+        router.get('/post/:id', async (req, res) => {
+            try{
+
+           let onePost= await Post.findOne({
+                    where: {
+                        id: req.params.id
+                    },
+                    attributes: [
+                        'id',
+                        'title',
+                        'content',
+                        'created_at'
+                    ],
+                    include: [{
+                            model: Comment,
+                            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                            include: {
+                                model: User,
+                                attributes: ['username']
+                            }
+                        },
+                        {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    ]
+                })
+                if (!onePost) {
+                    res.status(404).json({
+                        message: 'No post found with this id'
+                    });
+                    return;
+                }
+    
+                const post = onePost.get({plain: true});
+    
+                res.render('single-post', {
+                    post,
+                    loggedIn: req.session.loggedIn
+                });
+            }
+            catch(err){
+                res.status(500).json(err)
+            }
+            });
     
